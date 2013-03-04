804cfa685205425df18bbdbc0036a8c90eed0f4f
Stefan Foulis <stefan@foulis.ch>
2013-03-04 15:28:28 +0100

    drop some unused code

--- djangocms_accounts/urls.py
+++ djangocms_accounts/urls.py
@@ -35,20 +35,6 @@ urlpatterns = patterns('',
     url(r"^profile/email/(?P<pk>\d+)/delete/$", ProfileEmailDeleteView.as_view(), name="accounts_email_delete"),
     url(r"^profile/email/(?P<pk>\d+)/make_primary/$", ProfileEmailMakePrimaryView.as_view(), name="accounts_email_make_primary"),
 
-#    url(r"^signup/$", SignupView.as_view(), name="account_signup"), #
-#    url(r"^confirm_email/(?P<key>\w+)/$", ConfirmEmailView.as_view(), name="account_confirm_email"),
-#    url(r"^password/$", ChangePasswordView.as_view(), name="account_password"),
-#    url(r"^password/reset/$", PasswordResetView.as_view(), name="account_password_reset"),
-#    url(r"^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$", PasswordResetTokenView.as_view(), name="account_password_reset_token"),
-#
-#    url(r'^profile/$', ProfileView.as_view(), name="account_profile"),
-#    url(r'^profile/associations/$', ProfileAssociationChoicesView.as_view(), name="account_associations"),
-#    url(r"^email/$", SettingsView.as_view(), name="account_email"),
-#    url(r"^settings/$", SettingsView.as_view(), name="account_settings"),
-#    url(r"^delete/$", DeleteView.as_view(), name="account_delete"),
-#
-#
-#
     # Social Auth
     url(r'^login/(?P<backend>[^/]+)/$', social_auth.views.auth, name='socialauth_begin'),
     url(r'^complete/(?P<backend>[^/]+)/$', social_auth.views.complete, name='socialauth_complete'),
