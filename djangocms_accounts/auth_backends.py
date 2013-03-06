1b90f9dfb5f0cc6ebfdfffce0ab0e82ef258c083
Stefan Foulis <stefan@foulis.ch>
2013-03-06 16:22:48 +0100

    more fallbacks for authentication backend

--- djangocms_accounts/auth_backends.py
+++ djangocms_accounts/auth_backends.py
@@ -2,16 +2,27 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 from django.core.exceptions import MultipleObjectsReturned
-from djangocms_accounts.models import EmailAddress
+from djangocms_accounts.models import EmailAddress, EmailConfirmation
 
 
 class EmailBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
-        try:
-            # TODO: can we enforce email to be unique instead?
-            email = EmailAddress.objects.get(email=username)
+        """
+        tries verified email addresses, the email field on user objects and unconfirmed email addresses.
+        username is not checked, since the default model backend already does that.
+        """
+        username = username.strip()
+        # try verified email addresses
+        for email in EmailAddress.objects.filter(email=username):
+            # (EmailAddress.email is unique, but using the forloop vs a .get removes the need for a try/except.
             if email.user.check_password(password):
                 return email.user
-            return None
-        except (EmailAddress.DoesNotExist, User.DoesNotExist, MultipleObjectsReturned):
-            return None
+        # try the email field on the user
+        for user in User.objects.filter(email=username):
+            if user.check_password(password):
+                return user
+        # try unconfirmed email addresses
+        for email_confirmation in EmailConfirmation.objects.filter(email=username):
+            if email_confirmation.user.check_password(password):
+                return email_confirmation.user
+        return None
