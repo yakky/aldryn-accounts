410159f117a15a3ec05a0b396e5de6489ea96911
Stefan Foulis <stefan@foulis.ch>
2013-04-11 15:27:25 +0200

    removes confirm password field on password change

--- djangocms_accounts/forms.py
+++ djangocms_accounts/forms.py
@@ -42,10 +42,6 @@ class ChangePasswordForm(forms.Form):
         label=_("New Password"),
         widget=forms.PasswordInput(render_value=False)
     )
-    password_new_confirm = forms.CharField(
-        label=_("New Password (again)"),
-        widget=forms.PasswordInput(render_value=False)
-    )
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop("user")
@@ -55,12 +51,6 @@ class ChangePasswordForm(forms.Form):
         if not self.user.check_password(self.cleaned_data.get("password_current")):
             raise forms.ValidationError(_("Please type your current password."))
         return self.cleaned_data["password_current"]
-
-    def clean_password_new_confirm(self):
-        if "password_new" in self.cleaned_data and "password_new_confirm" in self.cleaned_data:
-            if self.cleaned_data["password_new"] != self.cleaned_data["password_new_confirm"]:
-                raise forms.ValidationError(_("You must type the same password each time."))
-        return self.cleaned_data["password_new_confirm"]
 
     def save(self, user):
         user.set_password(self.cleaned_data["password_new"])
