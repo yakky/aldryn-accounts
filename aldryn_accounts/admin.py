6f2f1dbc6e22b90340b0a76f83ef385f82ec570a
Stefan Foulis <stefan@foulis.ch>
2013-06-28 21:11:37 +0200

    adds more natural admin that hides username because it is irrelevant and show email instead

--- aldryn_accounts/admin.py
+++ aldryn_accounts/admin.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+import uuid
+from aldryn_accounts.admin_forms import UserCreationForm
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import EmailConfirmation, EmailAddress, UserSettings
 from social_auth.db.django_models import UserSocialAuth
+from django.utils.translation import ugettext, ugettext_lazy as _
+
 
 
 class EmailInline(admin.TabularInline):
@@ -14,6 +18,7 @@ class EmailInline(admin.TabularInline):
 class UserSocialAuthInline(admin.TabularInline):
     model = UserSocialAuth
     extra = 0
+    max_num = 0
 
 
 class UserSettingsInline(admin.StackedInline):
@@ -23,12 +28,51 @@ class UserSettingsInline(admin.StackedInline):
 
 
 class AccountsUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'social_logins',)
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'social_logins',)
     list_filter = ('is_staff', 'is_superuser', 'is_active')
     inlines = [UserSettingsInline, EmailInline, UserSocialAuthInline]
+    readonly_fields = UserAdmin.readonly_fields + ('email', 'last_login', 'date_joined')
+    add_readonly_fields = UserAdmin.readonly_fields + ('last_login', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined', 'username')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password',)}
+        ),
+        (_('Personal info'), {'fields': ('first_name', 'last_name',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+    )
+    add_form = UserCreationForm
+
 
     def social_logins(self, obj):
         return u", ".join([u"%s (%s)" % (i.provider, i.uid) for i in obj.social_auth.all()])
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.pk:
+            return self.readonly_fields
+        else:
+            # add form
+            return self.add_readonly_fields
+
+    def save_formset(self, request, form, formset, change):
+        """
+        Given an inline formset save it to the database.
+        """
+        # do the regular save
+        super(AccountsUserAdmin, self).save_formset(request, form, formset, change)
+        if not change and formset.model == EmailAddress:
+            # we are adding a new user and this is the EmailAddress formset
+            # make sure the email entered on the user is also added as the primary email
+            if formset.instance.email:
+                EmailAddress.objects.add_email(formset.instance, formset.instance.email, make_primary=True)
 
 
 class EmailConfirmationAdmin(admin.ModelAdmin):
@@ -42,8 +86,14 @@ class EmailConfirmationAdmin(admin.ModelAdmin):
 
 
 class UserProxy(User):
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = uuid.uuid4().get_hex()[:30]
+        return super(UserProxy, self).save(*args, **kwargs)
     class Meta:
         proxy = True
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
 
 admin.site.register(UserProxy, AccountsUserAdmin)
 admin.site.register(EmailConfirmation, EmailConfirmationAdmin)
---
+++ aldryn_accounts/admin_forms.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from aldryn_accounts.models import EmailAddress
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import MultipleObjectsReturned
+from django.utils.translation import ugettext, ugettext_lazy as _
+
+
+
+class UserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given email and
+    password.
+    """
+    error_messages = {
+        'duplicate_email': _("A user is already registered with this email address."),
+    }
+    email = forms.EmailField()
+    password = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_email(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        email = self.cleaned_data["email"]
+        try:
+            User._default_manager.get(email=email)
+            verified_qs = EmailAddress.objects.filter(email__iexact=email)
+            if verified_qs.exists():
+                raise forms.ValidationError(self.error_messages['duplicate_email'])
+        except User.DoesNotExist:
+            return email
+        except MultipleObjectsReturned:
+            raise forms.ValidationError(self.error_messages['duplicate_email'])
+        return email
+
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
