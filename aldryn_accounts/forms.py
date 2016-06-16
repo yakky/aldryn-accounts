ff8737eb9d0ed9870ba3ecfcec4f9f54c89dc2ef
Kirill Kniazev <kirill.kniazev@divio.ch>
2016-06-16 17:34:13 +0300

    fix typo

--- aldryn_accounts/forms.py
+++ aldryn_accounts/forms.py
@@ -164,7 +164,7 @@ class SignupForm(forms.Form):
 
 
 class SignupEmailResendConfirmationForm(forms.Form):
-    email = forms.EmailField(lable=_("E-mail"), required=True, widget=forms.HiddenInput())
+    email = forms.EmailField(label=_("E-mail"), required=True, widget=forms.HiddenInput())
 
     def clean_email(self):
         email = self.cleaned_data["email"]
